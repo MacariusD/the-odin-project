3216604607d28f58fcbf2994c276ef9af91effe1
Macarius <49547750+MacariusD@users.noreply.github.com>
2022-03-29 12:40:40 -0600

    Update scripts.js
    - computerPlay function completed

--- projects/javascript-basics/js/scripts.js
+++ projects/javascript-basics/js/scripts.js
@@ -29,4 +29,16 @@ function fizzBuzz() {
     } else {
         result.insertAdjacentText("beforeend", 'Please Enter a Valid Number');
     }
-}       
+}      
+
+function computerPlay() {
+    let maxNum = 3//exclusive
+    let minNum = 0 //inclusive
+    let computerOptions = ['Rock', 'Paper', 'Scissors']
+
+    return computerOptions[Math.floor(Math.random() * (maxNum - minNum)) + minNum];
+}
+
+
+
+console.log(computerPlay())
